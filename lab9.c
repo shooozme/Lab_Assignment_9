55cb368d20dd288b21b1974a11af6aa076c14ccb
shooozme <shooozme@gmail.com>
2024-04-12 19:01:05 -0400

    style: added comments

--- lab9.c
+++ lab9.c
@@ -90,16 +90,23 @@ void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
 			if(key < 0) {
 				
 			} else {
+				//prints formatiting
 				printf("\tindex %d -> ", i);
+				//prints first item in array
 				printf("%d, %c, %d", pHashArray[key].record->id, pHashArray[key].record->name, pHashArray[key].record->order);
+
+				//sets a temp variable for use in the folowing loop
 				struct HashType *temp = &pHashArray[key];
+				//loop checks all next elements to see if there is more items to be printed
 				while (temp->next != NULL) {
 					printf(" -> %d , %c, %d ", temp->next->record->id, 
 										  temp->next->record->name, 
 										  temp->next->record->order);
 					
+					//moves to next item in list if next is not NULL!
 					temp = temp->next;
 				}
+				//formatting
 				printf("\n");
 			}
 		}
@@ -115,31 +122,44 @@ int main(void)
 	printRecords(pRecords, recordSz);
 	// Your hash implementation
 	
+	//declares and allocates memory to pHashArray
 	struct HashType *pHashArray = (struct HashType*) malloc(sizeof(struct HashType) * recordSz);
 
+	//opens for loop for iterating thought array
 	for(int i = 0; i < recordSz; i++) {
 		
+		//declares and sets key for
 		int key = hash(pRecords[i].id);
+		//if the key is not matching then we can simply set the matching index from pRecords[] to it 
 		if(pHashArray[key].key != key) {
 			
 			pHashArray[key].record = (struct RecordType*)malloc(sizeof(struct RecordType));
+			//since this is the first in its index, the next pointer must be NULL
 			pHashArray[key].next = NULL;
 			pHashArray[key].key = key;
 			pHashArray[key].record->id = pRecords[i].id;
 			pHashArray[key].record->name = pRecords[i].name;
 			pHashArray[key].record->order = pRecords[i].order;
+			
+			//if the key is matching then we must move to next available next pointer
 		} else if (pHashArray[key].key == key ) {
 
+			//declare temp for use in while loop
 			struct HashType *temp = &pHashArray[key];
+			//open while loop and loop until next is NULL, this means we have an open spot in the linked list
 			while (temp->next != NULL) {
 				temp = temp->next;
 			}
+				//malloc all neccessary structs
 				temp->next = (struct HashType*)malloc(sizeof(struct HashType));
 				temp->next->record = (struct RecordType*)malloc(sizeof(struct RecordType));
+
+				//set all valid fields to its respective value found in the matching index from pRecords[]
 				temp->next->key = key;
 				temp->next->record->id = pRecords[i].id;
 				temp->next->record->name = pRecords[i].name;
 				temp->next->record->order = pRecords[i].order;
+				//set next to NULL because this must be the last item in the linked list!
 				temp->next->next = NULL;
 		}
 	}
